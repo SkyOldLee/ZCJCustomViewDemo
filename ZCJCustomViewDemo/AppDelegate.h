d5a7f2a71803c0114494c78156f4f501b0600c3c
zcj <405911774@qq.com>
2016-10-31 11:26:08 +0800

    Initial Commit

---
+++ ZCJCustomViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZCJCustomViewDemo
+//
+//  Created by zhangchaojie on 16/10/31.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZCJCustomViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZCJCustomViewDemo
+//
+//  Created by zhangchaojie on 16/10/31.
+//  Copyright © 2016年 zhangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
